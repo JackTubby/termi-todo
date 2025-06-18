3ec7064621b716467a1db322b269e8739ff85449
Jack Tubby <jack200034@hotmail.com>
2025-06-18 23:46:37 +0100

    Add TypeScript interfaces for TodoData and inquirer-datepicker-prompt module

---
+++ src/types/data.types.ts
@@ -0,0 +1,6 @@
+export interface TodoData {
+  id: string,
+  title: string,
+  description: string,
+  createdAt: string
+}
---
+++ src/types/inquirer-datepicker-prompt.d.ts
@@ -0,0 +1,5 @@
+declare module 'inquirer-datepicker-prompt' {
+  import { PromptConstructor } from 'inquirer'
+  const prompt: PromptConstructor
+  export default prompt
+}
