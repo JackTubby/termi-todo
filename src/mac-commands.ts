686a33b679f1fecdce3fcc958b196ae13528761b
Jack Tubby <jack200034@hotmail.com>
2025-06-19 08:46:22 +0100

    Enhance documentation for textToSpeech and displayDialog functions in mac-commands.ts

--- src/mac-commands.ts
+++ src/mac-commands.ts
@@ -1,5 +1,19 @@
 import { exec, spawn } from 'child_process'
 
+/**
+ * This function uses the `say` command to convert text to speech on macOS.
+ * It allows you to specify the voice and the rate of speech.
+ * The `timeout` parameter controls the speed of the speech, where a higher value means slower speech.
+ * If the `timeout` is set to 0, it will use the default rate.
+ * @param voice
+ * @param timeout
+ * @param message
+ * @example
+ * textToSpeech('Samantha', 200, 'Hello, this is a test message')
+ * @returns void
+ * @throws Will log an error message if the text-to-speech command fails.
+ * @see https://ss64.com/osx/say.html for more information on the `
+ */
 export function textToSpeech(voice: string = 'Samantha', timeout: number = 0, message: string) {
   const process = exec(`say -v ${voice} -r ${timeout} ${message}`)
   process.on('err', (err) => {
@@ -7,6 +21,21 @@ export function textToSpeech(voice: string = 'Samantha', timeout: number = 0, me
   })
 }
 
+/**
+ * Displays a dialog on macOS using AppleScript.
+ * This function uses `osascript` to run an AppleScript command that creates a dialog box with the specified title, message, and buttons.
+ * It returns a Promise that resolves with the button clicked by the user.
+ * If the user clicks "Cancel" or closes the dialog, it resolves with "Cancel".
+ * If the user clicks any other button, it resolves with the label of that button.
+ * @param title
+ * @param message
+ * @param buttons
+ * @returns
+ * @example
+ * displayDialog('Reminder', 'This is a reminder message', ['Cancel', 'OK'])
+ * @throws Will log an error message if the dialog command fails.
+ * @see https://developer.apple.com/library/archive/documentation/AppleScript/Conceptual/
+ */
 export function displayDialog(title: string, message: string, buttons: string[] = ['Cancel', 'OK']): Promise<string> {
   return new Promise((resolve) => {
     const buttonList = buttons.map((b) => `"${b}"`).join(', ')
